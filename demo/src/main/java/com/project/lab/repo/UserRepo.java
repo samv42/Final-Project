e8c9a544f6fd34b390d6b2b960090a33e0faf0c4
samve <sam.verner42@gmail.com>
2023-01-19 16:44:59 -0600

    test

--- demo/src/main/java/com/project/lab/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.project.lab.repo;
-
-import com.project.lab.CustomUserDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<CustomUserDetails, Long> {
-    CustomUserDetails findByUsername(String username);
-}
