3ee159f549ba794664d6e5ba3dd4902e85fe58bf
samve <sam.verner42@gmail.com>
2024-06-18 19:10:34 -0500

    deployment

--- src/main/java/com/project/lab/BudgetingAppApplication.java
+++ src/main/java/com/project/lab/BudgetingAppApplication.java
@@ -1,27 +1,9 @@
 package com.project.lab;
 
-import com.project.lab.models.Account;
-import com.project.lab.repo.AccountRepo;
-import com.project.lab.services.AccountService;
-import com.project.lab.models.Debt;
-import com.project.lab.services.DebtService;
-import org.hibernate.type.IdentifierBagType;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.config.BootstrapMode;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
-import org.thymeleaf.expression.Lists;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @SpringBootApplication
 @EnableCaching
